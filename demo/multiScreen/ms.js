0ba9bba187b3794b6a9d67517d2dcee95c9ee343
wzwxwc <475977851@qq.com>
2018-04-29 17:02:33 +0800

    umd来写新的模块

--- demo/multiScreen/ms.js
+++ demo/multiScreen/ms.js
@@ -13,11 +13,28 @@ window.onload = function () {
         source: "Satellite",
         type: "Map"
     });
+    var lyrGaoDe = layerUtil.tile({
+        server: "GaoDe",
+        source: "Satellite",
+        type: "Map"
+    });
+    var lyrGaoDeAnno = layerUtil.tile({
+        server: "GaoDe",
+        source: "Satellite",
+        type: "Annotion"
+    });
+    var lyrMapABC = layerUtil.tile({
+        server: "Google",
+        source: "Normal",
+        type: "Map"
+    });
+
     var lyrOSM = new ol.layer.Tile({
         source: new ol.source.OSM()
     });
 
-    var map1 = ol.Map({
+    //对map进行初始化的时候，不加new为啥会报错？
+    var map1 = new ol.Map({
         target: "map1",
         view: view1,
         layers: [
@@ -28,13 +45,39 @@ window.onload = function () {
         loadTilesWhileInteracting: false
 
     });
+    var map2 = new ol.Map({
+        target: "map2",
+        view: view1,
+        layers: [
+            lyrGaoDe,
+            lyrGaoDeAnno
+        ],
+        controls: ol.control.defaults({attribution: false, zoom: false, dragAble: false}),
+        loadTilesWhileAnimating: true,
+        loadTilesWhileInteracting: false
 
-    // var map = new ol.Map({
-    //     target: 'map1',
-    //     layers: [lyrGoogle],
-    //     view: view1,
-    //     controls: ol.control.defaults({attribution: false, zoom: false, dragAble: false}),
-    //     loadTilesWhileAnimating: true,
-    //     loadTilesWhileInteracting: false
-    // });
+    });
+    var map3 = new ol.Map({
+        target: "map3",
+        view: view1,
+        layers: [
+            lyrMapABC
+        ],
+        controls: ol.control.defaults({attribution: false, zoom: false, dragAble: false}),
+        loadTilesWhileAnimating: true,
+        loadTilesWhileInteracting: false
+
+    });
+    var map4 = new ol.Map({
+        target: "map4",
+        view: view1,
+        layers: [
+            lyrOSM
+        ],
+        controls: ol.control.defaults({attribution: false, zoom: false, dragAble: false}),
+        loadTilesWhileAnimating: true,
+        loadTilesWhileInteracting: false
+
+    });
+
 };
